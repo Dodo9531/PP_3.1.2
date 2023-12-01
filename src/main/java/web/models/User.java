b7c93b85ae986591cd443c72d544a94db25962c9
Dodo9531 <oge2782@mai.ru>
2023-12-01 15:05:54 +0300

    fix

--- src/main/java/web/models/User.java
+++ src/main/java/web/models/User.java
@@ -1,27 +1,24 @@
 package web.models;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Table(name = "Users")
 @Entity
-@Data
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Id")
     private int id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "surname")
     private String surname;
 
     public User() {
     }
-
-    ;
-
     public User(int id, String name, String surname) {
         this.id = id;
         this.name = name;
